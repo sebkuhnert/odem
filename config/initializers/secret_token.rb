b76ffede751ba838b50cc1f6fa68db35d850d6ab
sebkuhnert <kuhnertsebastian@gmail.com>
2014-03-27 18:07:20 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odem::Application.config.secret_key_base = '4c8ceb6a71f93ae3532e3f1fd3e3fe95bbb859237a2bf770e3ecc19f70d300cadd2c873e80a8be9e7f8c0be88c8b9988081888778b9154e37466f4751be93771'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Odem::Application.config.session_store :cookie_store, key: '_odem_session'
